e8765e0b2b3bcf3eb382c5c6156d0798331dea11
vps <vps@www4242gi.sakura.ne.jp>
2013-09-20 01:17:21 +0900

    Apply bootstrap thema

--- app/models/concerns/check_url.rb
+++ app/models/concerns/check_url.rb
@@ -1,8 +1,9 @@
 require 'nokogiri'
+require 'open-uri'
 module CheckURL
   def check_url(url)
       begin
-        doc = Nokogiri::HTML(open(url),nil,'utf-8')
+        doc = Nokogiri::HTML(open(url).read,nil,'utf-8')
         return true
       rescue => ex
         return false
---
+++ vendor/bundler/ruby/2.0.0/gems/jquery-ui-rails-4.0.4/lib/jquery/ui/rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Ui
+    module Rails
+      VERSION = "4.0.4"
+    end
+  end
+end
