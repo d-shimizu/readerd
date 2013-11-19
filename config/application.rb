350767ecf22ab200d8056a537f87c4f56bdb0be1
vps <vps@www4242gi.sakura.ne.jp>
2013-11-20 00:21:53 +0900

    Modified to hide the sidebar if the window size is 768px or less by using a media query

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+### newrelic
+require 'newrelic_rpm'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -15,6 +18,10 @@ module Readerd
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    ### ActiveRecord timzone
+    ### http://twodollarz.hatenablog.jp/entry/20120703
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
